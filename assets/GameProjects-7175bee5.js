82c9f6a6c22fe07699950ac57de6d6ee96e133d9
Kadrius <adrian.blanco@xoia.es>
2024-05-25 22:40:51 +0200

    + Added About View + Added Texas project

---
+++ assets/GameProjects-7175bee5.js
@@ -0,0 +1,145 @@
+import{P as e,a as r}from"./ProjectData-7cf3ef80.js";import{f as i,_ as o,r as n,c,d as l,a,b as d,o as p}from"./index-9342e3d3.js";const h=[new e("project-1","3D Wind Training","img/projects/3DWindTraining-Header.jpg",`
+    <div class="paragraph">
+     <strong>3D Wind Training </strong> is a VR Training tool for wind turbine assembly workers. It's divided into two parts, one part (The practice) where the operator is trained in how to do the process and another one (The exam) where the knowledge of the user is tested.
+     The results of the exam (and other relevant data) are stored in a CMS that the teachers can use to check how the trainees are progressing. The system was designed so it was easy to extend and add more training process with different tools and industrial sectors.
+     <br/>My role on the project was as lead (and only) developer. I designed and coded all the functionalities, from the library that contains the Training System to the design and deployment of the CMS and the communication logic with the app.  
+    </div>
+    <div class="paragraph center">
+        <iframe class="youtube" src="https://www.youtube.com/embed/OmTzBVijcCo" frameborder="0" allowfullscreen></iframe>
+    </div>
+    <!--div class="paragraph center">
+        <a href="https://play.google.com/store/apps/details?id=some.playstore.thing" target="_blank"><img src="img/projects/play-store-logo.png" alt="Play Store badge" /></a>
+    </div-->
+
+    <div class="paragraph">
+        Main features :
+        <ul>
+        <li>Modular solution, can be easily extendable and used in other projects.</li>
+        <li>Allows to track traineed progression.</li>
+        <li>Trains workers to work with dangerous machinery so it helps to reduce work related injuries.</li>
+        <li>It's intuitive and easy to use.</li>
+        </ul>
+    </div>
+
+    <div class="paragraph center">
+        <img class="pc-screenshot" src="img/projects/3DWindTraining/cap_texas1.jpg" alt="3D Wind Training Screenshot" />
+        <img class="pc-screenshot" src="img/projects/3DWindTraining/cap_texas3.jpg" alt="3D Wind Training Screenshot" />
+    </div>
+    `,"#23bd69",!0),new e("project-2","Singing Addict","img/projects/project-2-icon.png",`
+    <div class="paragraph">
+        <strong>Optimistic Chubby Blog</strong> is a thing of beauty that I am so proud of. I could write about it for hours.
+        <br/>Image by <a target="_blank" href="https://www.pexels.com/fr-fr/@adonyi-gabor-604571">Adonyi Gábor</a>.
+    </div>
+    <div class="paragraph center">
+        <iframe class="youtube" src="https://www.youtube.com/embed/dQw4w9WgXcQ" frameborder="0" allowfullscreen></iframe>
+    </div>
+
+    <div class="paragraph">
+        Main features :
+        <ul>
+        <li>Some stuff</li>
+        <li>Some great stuff</li>
+        <li>More awesome stuff</li>
+        <li>And then some</li>
+        </ul>
+    </div>
+
+    <div class="paragraph">
+    <div class="notice">
+        Windows build available on <a href="https://some.where/nice" target="_blank">itch.io</a>.
+        Source code is available on <a href="https://github.com/yourself" target="_blank">GitHub</a>.
+    </div>
+    </div>
+
+    <div class="paragraph center">
+    <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Singing Addict Screenshot" />
+    <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Singing Addict Screenshot" />
+    <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Singing Addict Screenshot" />
+    <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Singing Addict Screenshot" />
+    </div>
+    `,"#5a78af"),new e("project-3","Drawing Overload","img/projects/project-3-icon.png",`
+    <div class="paragraph">
+        <strong>Drawing Overload</strong> is a thing of beauty that I am so proud of. I could write about it for hours.
+        <br/>Image by <a target="_blank" href="https://www.pexels.com/fr-fr/@miphotography">Miesha Maiden</a>.
+    </div>
+    <div class="paragraph center">
+        <iframe class="youtube" src="https://www.youtube.com/embed/dQw4w9WgXcQ" frameborder="0" allowfullscreen></iframe>
+    </div>
+
+    <div class="paragraph">
+        Main features :
+        <ul>
+        <li>Some stuff</li>
+        <li>Some great stuff</li>
+        <li>More awesome stuff</li>
+        <li>And then some</li>
+        </ul>
+    </div>
+
+    <div class="paragraph">
+        <div class="notice">
+        Playable in the browser (WebGL) on <a href="https://some.where/nice" target="_blank">itch.io</a>.
+        Source code is available on <a href="https://github.com/yourself" target="_blank">GitHub</a>.
+        </div>
+    </div>
+
+    <div class="paragraph center">
+        <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Drawing Overload Screenshot" />
+        <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Drawing Overload Screenshot" />
+        <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Drawing Overload Screenshot" />
+        <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Drawing Overload Screenshot" />
+    </div>
+    `,"#383838"),new e("project-4","Eugeneable","img/projects/project-4-icon.png",`
+    <div class="paragraph">
+    <strong>Eugeneable</strong> is a thing of beauty that I am so proud of. I could write about it for hours.
+    <br/>Image by <a target="_blank" href="https://www.pexels.com/fr-fr/@neo8iam">NEOSiAM 2020</a>.
+    </div>
+    
+    <div class="paragraph">
+        Main features :
+        <ul>
+        <li>Some stuff</li>
+        <li>Some great stuff</li>
+        <li>More awesome stuff</li>
+        <li>And then some</li>
+        </ul>
+    </div>
+
+    <div class="paragraph">
+        <div class="notice">
+        Windows build available on <a href="https://some.where/nice" target="_blank">itch.io</a>.
+        </div>
+    </div>
+
+    <div class="paragraph center">
+        <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Eugeneable Screenshot" />
+        <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Eugeneable Screenshot" />
+    </div>
+    `,"#e80fb7"),new e("project-5","Cloud Drew Land","img/projects/project-5-icon.png",`
+    <div class="paragraph">
+        <strong>Cloud Drew Land</strong> is a thing of beauty that I am so proud of. I could write about it for hours.
+        <br/>Image by <a target="_blank" href="https://www.pexels.com/fr-fr/@cottonbro">cottonbro</a>.
+    </div>
+    
+    <div class="paragraph">
+        Main features :
+        <ul>
+        <li>Some stuff</li>
+        <li>Some great stuff</li>
+        <li>More awesome stuff</li>
+        <li>And then some</li>
+        </ul>
+    </div>
+
+    <div class="paragraph">
+        <div class="notice">
+        Windows build available on <a href="https://some.where/nice" target="_blank">itch.io</a>.
+        Source code available on <a href="https://github.com/yourself" target="_blank">GitHub</a>.
+        </div>
+    </div>
+
+    <div class="paragraph center">
+        <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Cloud Drew Land Screenshot" />
+        <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Cloud Drew Land Screenshot" />
+        <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Cloud Drew Land Screenshot" />
+    </div>`,"#e48246")],g=i({name:"GameProjects",components:{ProjectsList:r},data:function(){return{projects:h}}}),m=a("h1",null,"Stuff",-1),f=a("div",{style:{"margin-bottom":"30px"}}," The following are some stuff I've made or heavily contributed to. ",-1),u=a("div",{style:{"margin-top":"20px"}},[d(" There is more to see on "),a("a",{target:"_blank",href:"https://someexternalwebsite.com"},"some external website")],-1);function b(t,w,v,y,S,j){const s=n("ProjectsList");return p(),c("div",null,[m,f,l(s,{projects:t.projects},null,8,["projects"]),u])}const x=o(g,[["render",b]]);export{x as default};
